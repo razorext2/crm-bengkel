31cf9430c209a1a56b1795e0ba150ec0af332d5e
razorext2 <razorext2@gmail.com>
2026-02-07 09:25:29 +0700

    ubah tombol di hero

--- resources/views/layouts/app/hero.blade.php
+++ resources/views/layouts/app/hero.blade.php
@@ -11,20 +11,23 @@
                 dan mulailah berbelanja.
             </p>
 
-            <a href="#"
-                class="mr-3 inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                Mulai
-                <svg class="-mr-1 ml-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd"
-                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clip-rule="evenodd"></path>
-                </svg>
-            </a>
-
-            <a href="{{ route('login') }}"
-                class="inline-flex items-center justify-center rounded-lg border border-gray-300 px-5 py-3 text-center text-base font-medium text-gray-900 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                Masuk
-            </a>
+            @if (auth()->check())
+                <a href="#"
+                    class="mr-3 inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                    Mulai
+                    <svg class="-mr-1 ml-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </a>
+            @else
+                <a href="{{ route('login') }}"
+                    class="inline-flex items-center justify-center rounded-lg border border-gray-300 bg-green-500 px-5 py-3 text-center text-base font-medium text-gray-900 hover:bg-green-100 focus:ring-4 focus:ring-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-green-700 dark:focus:ring-gray-800">
+                    Masuk
+                </a>
+            @endif
         </div>
 
         <div class="hidden lg:col-span-5 lg:mt-0 lg:flex">
